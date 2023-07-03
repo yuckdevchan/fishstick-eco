a85b2f461f06aec8f87826c8555d4cc867a31abe
manSlayer16 <120093141+manSlayer16@users.noreply.github.com>
2023-07-03 11:26:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-# tree oxygen output calculator ;D
-
-circumference = float(input("enter circumference of mature oak tree in inches: "))
-
-tree_circs = {7.9: 1, 19.7 : 5.3, 35.4: 10.3, 59.1: 17.5, 74.8: 24.6, 98.4: 37.4}
-
-for item in tree_circs.keys():
-  print(item)
-  if float(item) > circumference:
-    print(f"using data for {item} inches circumference")
-    break
-
-print(f"\nyour tree will produce approximately: {tree_circs[item]}kg of oxygen in 1 year.")    
+playerint = int(input("what is your tree circumferance in inches? "))
+print("your oxygen output for the tree is: ")
+print(playerint * 0.4)
