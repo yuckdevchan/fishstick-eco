db14f6ad564dda6dc2fa35ce729ddd5aa424bfc1
yuckdevchan <60288171+yuckdevchan@users.noreply.github.com>
2023-09-04 13:20:06 +0100

    prototype new version (math may be innacurate - cry about it)

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-tree_circ = input("What is your tree circumferance in inches? ")
-oxygen_output_kg = float(tree_circ) * 0.4
-print(f"Your yearly oxygen output for the tree is: {oxygen_output_kg}")
+# You must use python 3.6 or later to run this program.
+time_units = {"1": "days", "2": "weeks", "3": "months", "4": "years"}
+time_unit = input("Do you measure your tree's circumference:\n1. Every Day\n2. Every Week\n3. Every Month\n4. Every Year\n\nInput: ")
+
+if time_unit not in time_units:
+  while time_unit not in time_units:
+    time_unit = input("Invalid Choice!\n\nTry Again: ")
+
+amount_of_time = input(f"How many {time_units[time_unit]} have you been measuring your tree's circumference? ")
+
+if not amount_of_time.isnumeric():
+  while not amount_of_time.isnumeric():
+    amount_of_time = input("Invalid Choice!\n\nTry Again: ")
+
+tree_circ_total = 0
+
+for i in range(1, int(amount_of_time) + 1):
+  tree_circ = input(f"What was your tree's circumference in {time_units[time_unit]} {i}? ")
+
+  if not tree_circ.isnumeric():
+    while not tree_circ.isnumeric():
+      tree_circ = input("Invalid Choice!\n\nTry Again: ")
+
+  tree_circ_total = tree_circ_total + float(tree_circ)
+
+if time_units[time_unit] is "years":
+  oxygen_output_kg = float(tree_circ_total) * 0.4
+print(f"Your yearly oxygen output for the tree is approximately: ~{oxygen_output_kg}kg.")
+
