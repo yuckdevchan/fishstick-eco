a87d920e8ff712463579513a32d326570e1170fd
yuckdevchan <60288171+yuckdevchan@users.noreply.github.com>
2023-09-04 12:51:42 +0100

    *yearly

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 tree_circ = input("What is your tree circumferance in inches? ")
 oxygen_output_kg = float(tree_circ) * 0.4
-print(f"Your oxygen output for the tree is: {oxygen_output_kg}")
+print(f"Your yearly oxygen output for the tree is: {oxygen_output_kg}")
