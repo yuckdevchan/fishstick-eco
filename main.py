5fd262369146d0cc07a381b7a32557aad8daf061
yuckdevchan <60288171+yuckdevchan@users.noreply.github.com>
2023-09-14 18:09:14 +0100

    simulation stuff :muscle:

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
-import random, time
-
 def team_trees():
     int(input("How many trees do you plant? "))
 
+
 def tardis():
     time_units = {
-        "days":1, 
-        "weeks":7, 
-        "months":31, 
-        "years":365, 
-        "decades":3650
+        "days": 1,
+        "weeks": 7,
+        "months": 31,
+        "years": 365,
+        "decades": 3650
     }
-    
-    handbrake = input("How much time do you let pass? e.g: '3 years', '2 days', '10 decades' ")
 
-def init_bruv():
+    handbrake = input("How much time do you let pass? e.g: '3 years', '2 days', '10 decades' ")
+    handbrake = handbrake.split(" ")
+    time_unit = handbrake[1]
+    time_amount = int(handbrake[0])
+    time_passed = time_units[time_unit] * time_amount  # find time passed in days
+
+
+def init_():
     team_trees()
     tardis()
 
-init_bruv()
+
+init_()
